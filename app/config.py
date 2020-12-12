7b67e7860401ce9b243039df59e857289491119c
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2020-12-12 22:57:54 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ import os
 
 
 try:
-    port = int(os.environ.get("PORT", "8080"))
+    port = 8080
 except ValueError:
     port = -1
 if not 1 <= port <= 65535:
@@ -14,8 +14,8 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["API_ID"])
-    api_hash = os.environ["API_HASH"]
+    api_id = 1064864
+    api_hash = "5f3eeab0e6108731551e6a93598b654c"
 except (KeyError, ValueError):
     traceback.print_exc()
     print("\n\nPlease set the API_ID and API_HASH environment variables correctly")
@@ -31,13 +31,13 @@ except:
     sys.exit(1)
 
 try:
-    session_string = os.environ["SESSION_STRING"]
+    session_string = "1BVtsOMQBuysvXwTKTYlsi4OnkWR7a5_CLDHUzbX4ZbdocDGCzOevstpsrTBGrDERO_SxHIyPqAjdbRx5ACz1XKOpzhKCaO8iJMgdidcut7oRIXHJ-gR4uLOK79FToWvklBEgNuni2YD8ffU2rgJe54z0Z77RBygrRifHXhHJm77_Pcct2eeoxow-XercU2k6CuJedn6DZ722GOsG-o6zifcN5dIq40Url01eEuw8uRqn-v9gzyHXAUrjiErwTKZbWuz-e9Izxde4xZOZbTmpWI3okqdaAYBIC1dwaBN6nFN5GGswqCMuc0yN7rTla-5OEGtYAvRnqrQq1iz_Bz5epV6KHna-HJU="
 except (KeyError, ValueError):
     traceback.print_exc()
     print("\n\nPlease set the SESSION_STRING environment variable correctly")
     sys.exit(1)
 
-host = os.environ.get("HOST", "0.0.0.0")
+host = 162.241.87.250
 debug = bool(os.environ.get("DEBUG"))
 block_downloads = bool(os.environ.get("BLOCK_DOWNLOADS"))
 results_per_page = int(os.environ.get("RESULTS_PER_PAGE", "20"))
